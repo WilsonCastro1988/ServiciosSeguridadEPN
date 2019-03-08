bf4c570a5198cd7a738535786e9a27898f9fe595
Edistiven <esu.matabay@yavirac.edu.ec>
2019-03-08 18:03:09 -0500

    Creación de la interfaz de Estado Producto

--- ejbModule/ec/edu/epn/laboratoriosBJ/beans/EstadoProductoDAO.java
+++ ejbModule/ec/edu/epn/laboratoriosBJ/beans/EstadoProductoDAO.java
@@ -1,13 +1,13 @@
 package ec.edu.epn.laboratoriosBJ.beans;
 
-import java.util.List;
 import javax.ejb.Local;
+
+import ec.edu.epn.generic.DAO.DaoGenerico;
 import ec.edu.epn.laboratorioBJ.entities.Estadoproducto;
 
-
 @Local
-public interface EstadoProductoDAO {
 
-	List<Estadoproducto> listaProducto(String nombreEstp);
+public interface EstadoProductoDAO extends DaoGenerico<Estadoproducto> {
 
+	
 }
